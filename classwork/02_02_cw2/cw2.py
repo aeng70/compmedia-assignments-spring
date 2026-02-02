09e88390cb86b82cd5dcec7d14b1ae2e8ea6ad9d
aeng70 <engarchibald@gmail.com>
2026-02-02 09:52:44 -0500

    cw

--- classwork/02_02_cw2/cw2.py
+++ classwork/02_02_cw2/cw2.py
@@ -8,3 +8,9 @@ def draw():
     square(348,1,50)
     square(1,348,50)
     square(348,348,50)
+    
+    #middle
+    square(149,149,50)
+    square(199,149,50)
+    square(149,199,50)
+    square(199,199,50)
