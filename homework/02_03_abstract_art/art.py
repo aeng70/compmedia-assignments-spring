9ba76d3c8ea9023361801c8e0ed7efc771b995e0
aeng70 <engarchibald@gmail.com>
2026-02-04 09:46:23 -0500

    art

--- homework/02_03_abstract_art/art.py
+++ homework/02_03_abstract_art/art.py
@@ -1,6 +1,16 @@
-def steup():
+def setup():
     size(400,400)
     
 def draw():
-    background(0,0,255)
+    background(255)
     
+    fill(112, 163, 204)
+    square(99,99,200)
+    
+    fill(220,0,0)
+    triangle(359, 30, 289, 359, 200, 45)
+    
+    fill(0,200,0)
+    circle(175,254,100)
+    
+    line(99,299,323,200)
