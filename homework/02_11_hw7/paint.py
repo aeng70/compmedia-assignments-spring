a7a0cd01427a6548b9b12c9ac80ecc70fa011f90
aeng70 <engarchibald@gmail.com>
2026-02-12 10:13:20 -0500

    sdf

--- homework/02_11_hw7/paint.py
+++ homework/02_11_hw7/paint.py
@@ -1,8 +1,9 @@
 colors = {}
 
 def setup():
-    size(600,400)
+    size(800,800)
     color_mode(HSB,360,100,100)
+    background(0,0,100)
     global colors
     colors = {
         "red":0,
@@ -10,19 +11,80 @@ def setup():
         "yellow":60,
         "green":120,
         "blue":240,
-        "indigo":270,
-        "violet":300 }
+        "purple":270 }
+    
+    global key_functions
+    key_functions = {
+        "clear":'c',
+        "small":'1',
+        "medium":'2',
+        "large":'3' }
     
 def draw():
-    background(0,0,100)
     fill(0,0,75)
-    rect(0,0,60,399)
+    rect(0,0,60,285)
     
+    fill(colors["red"], 100, 100)
     circle(30,30,30)
+    
+    fill(colors["orange"], 100, 100)
     circle(30,75,30)
+    
+    fill(colors["yellow"], 100, 100)
     circle(30,120,30)
+    
+    fill(colors["green"], 100, 100)
     circle(30,165,30)
+    
+    fill(colors["blue"], 100, 100)
     circle(30,210,30)
+    
+    fill(colors["purple"], 100, 100)
     circle(30,255,30)
-    circle(30,300,30)
+    
+    if is_mouse_pressed:
+        line(pmouse_x, pmouse_y, mouse_x, mouse_y)
         
+    if is_key_pressed:
+        if key == key_functions["clear"]:
+            background(0,0,100)
+        if key == key_functions["small"]:
+            stroke_weight(1)
+        if key == key_functions["medium"]:
+            stroke_weight(10)
+        if key == key_functions["large"]:
+            stroke_weight(20)
+        
+def mouse_pressed():
+    if collidePointCircle(mouse_x, mouse_y, 30, 30, 30):
+        stroke(colors["red"], 100, 100)
+        
+    if collidePointCircle(mouse_x, mouse_y, 30, 75, 30):
+        stroke(colors["orange"], 100, 100)
+        
+    if collidePointCircle(mouse_x, mouse_y, 30, 120, 30):
+        stroke(colors["yellow"], 100, 100)
+        
+    if collidePointCircle(mouse_x, mouse_y, 30, 165, 30):
+        stroke(colors["green"], 100, 100)
+        
+    if collidePointCircle(mouse_x, mouse_y, 30, 210, 30):
+        stroke(colors["blue"], 100, 100)
+        
+    if collidePointCircle(mouse_x, mouse_y, 30, 255, 30):
+        stroke(colors["purple"], 100, 100)
+    
+def collidePointCircle(pointX, pointY, circX, circY, diameter):
+  """Input coordinates for the point and x, y, and diameter (the width/height) of the circle.
+  Returns true if the point and circle are touching.
+  
+  Does not work for ellipse/oval shapes."""
+  
+  distance = dist(pointX, pointY, circX, circY)
+  radius = diameter/2
+  
+  if(distance <= radius):
+    return True
+  else:
+    return False
+    
