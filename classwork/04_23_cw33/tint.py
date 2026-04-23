202aa39390b5aa1bd7f37926dd7cca86c8b902df
aeng70 <engarchibald@gmail.com>
2026-04-23 10:15:14 -0400

    tint

--- classwork/04_23_cw33/tint.py
+++ classwork/04_23_cw33/tint.py
@@ -7,6 +7,14 @@ def setup():
     image(img, 0, 0)
     
     load_pixels()
-    h1 = get_pixels(0, 0, width // 2, height)
-    tint(255, 0, 0)
+    h1 = get_pixels(0, 0, width // 2, height // 2)
+    tint(255, 0, 0, 100)
     image(h1, 0, 0)
+    
+    h2 = get_pixels(width //2, 0, width, height // 2)
+    tint(0, 255, 0, 100)
+    image(h2, width // 2, 0)
+    
+    h3 = get_pixels(0, height // 2, width // 2, height)
+    tint(0, 0, 255, 100)
+    image(h3, 0, height // 2)
