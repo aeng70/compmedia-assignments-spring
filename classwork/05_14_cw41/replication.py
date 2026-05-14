766787fe08ab894e75776703a7fab07e1c87dcd8
aeng70 <engarchibald@gmail.com>
2026-05-14 10:14:48 -0400

    game of life

---
+++ classwork/05_14_cw41/replication.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def setup_pixels():
+    #code for initial state
+    for x in range(width):
+        for y in range(height):
+            chance = (int)(random(10))
+            if chance == 0:
+                np_pixels[y, x, 2] = 255
+            
+    update_np_pixels()
+            
+            
+    
+def setup():
+    global s
+    size(200,200)
+    s = [ [ ] ]  # state
+    background(0) # black
+    load_np_pixels()
+    setup_pixels()
+
+def evaluate_neighbors():
+    global s 
+    #code to update neighbors-state
+
+def transition():
+    #code to apply the rules from
+    #neighbors’ state
+    update_np_pixels()
+
+def draw(): # driver
+    evaluate_neighbors()
+    transition()
+
